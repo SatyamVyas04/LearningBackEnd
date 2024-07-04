d21c06d077a07d0b218b1ef1ddc2290135f79ddc
SatyamVyas <vyas.satyam.04@gmail.com>
2024-07-04 16:40:01 +0530

    Template Project Finalised

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+connectDB()
+    .then(() => {
+        const port = process.env.PORT || 8000;
+        app.on("Error", (error) => {
+            console.log("APP ERROR:", error);
+            throw error;
+        });
+        app.listen(port, () => {
+            console.log("Server is running at port:", port);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB CONNECTION FAILED:", err);
+    });
