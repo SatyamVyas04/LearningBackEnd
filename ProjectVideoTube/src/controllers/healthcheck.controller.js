e5e03ae235393607daa7372944b33fa95a4aacc2
SatyamVyas <vyas.satyam.04@gmail.com>
2024-07-16 22:52:11 +0530

    Completed Further

--- ProjectVideoTube/src/controllers/healthcheck.controller.js
+++ ProjectVideoTube/src/controllers/healthcheck.controller.js
@@ -4,6 +4,11 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    try {
+        res.status(200, new ApiResponse(200, {}, "Server is running fine!"));
+    } catch (error) {
+        throw new ApiError(501, {}, "Server Error! Please try again later");
+    }
 });
 
 export { healthcheck };
