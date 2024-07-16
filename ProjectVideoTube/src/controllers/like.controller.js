7b69765fa4dc702d187c0a82b485234fc0f088f1
SatyamVyas <vyas.satyam.04@gmail.com>
2024-07-17 01:29:49 +0530

    Complete likes controller

--- ProjectVideoTube/src/controllers/like.controller.js
+++ ProjectVideoTube/src/controllers/like.controller.js
@@ -4,23 +4,132 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const toggleVideoLike = asyncHandler(async (req, res) => {
+const toggleVideoLike = asyncHandler(async (req, res, next) => {
     const { videoId } = req.params;
-    //TODO: toggle like on video
+    if (!videoId) throw new ApiError(404, "Video not found");
+
+    try {
+        const preLiked = await Like.findOneAndDelete({
+            video: videoId,
+            likedBy: req.user._id,
+        });
+
+        if (preLiked) {
+            return res
+                .status(200)
+                .json(new ApiResponse(200, null, "Unliked successfully"));
+        }
+
+        const like = await Like.create({
+            video: videoId,
+            likedBy: req.user._id,
+        });
+
+        if (!like)
+            throw new ApiError(500, "Internal Server Error while liking");
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, like, "Like posted successfully"));
+    } catch (error) {
+        throw new ApiError(500, `Internal Server Error: ${error.message}`);
+    }
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
-    //TODO: toggle like on comment
+    if (!commentId) throw new ApiError(404, "Comment not found");
+
+    try {
+        const preLiked = await Like.findOneAndDelete({
+            comment: commentId,
+            likedBy: req.user._id,
+        });
+
+        if (preLiked) {
+            return res
+                .status(200)
+                .json(new ApiResponse(200, null, "Unliked successfully"));
+        }
+
+        const like = await Like.create({
+            comment: commentId,
+            likedBy: req.user._id,
+        });
+
+        if (!like)
+            throw new ApiError(500, "Internal Server Error while liking");
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, like, "Like posted successfully"));
+    } catch (error) {
+        throw new ApiError(500, `Internal Server Error: ${error.message}`);
+    }
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
-    //TODO: toggle like on tweet
+    if (!tweetId) throw new ApiError(404, "Tweet not found");
+
+    try {
+        const preLiked = await Like.findOneAndDelete({
+            tweet: tweetId,
+            likedBy: req.user._id,
+        });
+
+        if (preLiked) {
+            return res
+                .status(200)
+                .json(new ApiResponse(200, null, "Unliked successfully"));
+        }
+
+        const like = await Like.create({
+            tweet: tweetId,
+            likedBy: req.user._id,
+        });
+
+        if (!like)
+            throw new ApiError(500, "Internal Server Error while liking");
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, like, "Like posted successfully"));
+    } catch (error) {
+        throw new ApiError(500, `Internal Server Error: ${error.message}`);
+    }
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    try {
+        const aggregatePipeline = [
+            {
+                $match: { likedBy: req.user._id },
+            },
+            {
+                $lookup: {
+                    from: "videos",
+                    localField: "video",
+                    foreignField: "_id",
+                    as: "videos",
+                },
+            },
+        ];
+
+        const likedVideos = Like.aggregate(aggregatePipeline);
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    likedVideos,
+                    "Liked videos fetched successfully"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, `Internal Server Error: ${error.message}`);
+    }
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
